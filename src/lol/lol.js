9cab81faa7402e936c987c6bd7adce797a38a0a7
Santiago Díaz Guevara <santidiazg@hotmail.es>
2021-11-23 18:01:50 -0700

    Reduce API call failure error message

--- src/lol/lol.js
+++ src/lol/lol.js
@@ -11,7 +11,7 @@ class MatchAPI {
         const [response, error] = await handle(axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/${matchID}?api_key=${process.env.API_KEY}`));
 
         if (error) {
-            console.log(error);
+            console.log(`Failed to get match data through Riot's API: ${error.response.status} ${error.response.statusText}`);
 
             return null;
         }
